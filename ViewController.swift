e170881edbd0c2d0fa6bc0f916cd53b23379b620
aksh0124 <aksh271@gmail.com>
2018-05-27 23:51:17 -0700

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PiggyBot
+//
+//  Created by Anant Soft Computing on 24/05/18.
+//  Copyright © 2018 Anant Soft Computing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
